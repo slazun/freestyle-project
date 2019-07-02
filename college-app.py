d803162eb6375bd0816260fd0f5dda0b3a8ab13c
Sarah Lazun <sel402@stern.nyu.edu>
2019-07-02 12:39:00 -0400

    added comparisons of user inputs vs. avg on diversity attributes

--- college-app.py
+++ college-app.py
@@ -38,8 +38,8 @@ budget = input("Do you have a budget restriction? Please enter 'Yes' or 'No':")
 if budget == 'Yes':
     budget_amount = input("Please input a max yearly tuition amount like '50000':") #TODO make tuition number user friendly
 elif budget == 'No':
-    print("LUCKY YOU!" + " " + emoji.emojize(":clap:"))
-    budget_amount == 10000000000000
+    print("LUCKY YOU!" + " " + emoji.emojize(":expressionless_face:"))
+    budget_amount = 10000000000000
 else:
     print("Sorry that is an inalid input. Please try again with 'Yes' or 'No'.")
     exit()
@@ -48,8 +48,8 @@ SAT_score = input("What is your SAT score out of 1600? If you don't mind me aski
 
 #Defining benchmarks https://www.census.gov/library/visualizations/2018/comm/classroom-diversity.html
 
-avg_percent_white == 0.547 
-avg_percent_female == 0.559
+avg_percent_white = 0.547 
+avg_percent_female = 0.559
 avg_percent_first_gen = 0.3 #source http://www.firstgenerationfoundation.org/
 avg_percent_veteran = 0.04 #source https://www.acenet.edu/the-presidency/columns-and-features/Pages/By-the-Numbers-Undergraduate-Student-Veterans.aspx
 
@@ -75,19 +75,61 @@ print(type(parsed_response['results'])) #list
 print(parsed_response['results'][0]) 
 print(type(parsed_response['results'][0])) #dictionary 
 
-#Output for single college search 
+#Output for single college search. Basic Info. Source credit: robo advisor project
 
 print("-------------------------")
 print("POTENTIAL COLLEGE:" + " " + str(school_name))
 print("COLLEGE LOCATION:"+ " " + str(parsed_response['results'][0]['school.city'])+ ", " +str(parsed_response['results'][0]['school.state']))
-print("STUDENT POPULATION SIZE:" + " " + str(parsed_response['results'][0]['latest.student.size']) + " " + emoji.emojize(":grimacing_face:"))
+print("STUDENT POPULATION SIZE:" + " " + str(parsed_response['results'][0]['latest.student.size']))
+print("-------------------------")
+
+#Assessing if attendance is realistic
+
 if student_state == parsed_response['results'][0]['school.state']:
-    tuition = parsed_response['results'][0]['latest.cost.tuition.in_of_state']
+    tuition = parsed_response['results'][0]['latest.cost.tuition.in_state']
 else:
     tuition = parsed_response['results'][0]['latest.cost.tuition.out_of_state']
 
 if float(tuition) > float(budget_amount):
-    print("TUITION IS:" + " " + str(to_usd(float(latest_close))+ ". " + "" )
+    print("YEARLY TUITION:" + " " + str(to_usd(float(tuition)))+ ". " + "This school is outside of your budget." + " " + emoji.emojize(":worried_face:"))
+elif float(tuition) <= float(budget_amount):
+    print("YEARLY TUITION:" + " " + str(to_usd(float(tuition)))+ ". " + " " + emoji.emojize(":money_with_wings:"))
+else:
+    pass
+
+avg_SAT_score = int(parsed_response['results'][0]['latest.admissions.sat_scores.average.overall'])
+
+if avg_SAT_score > (int(SAT_score) + 50):
+    print("AVERAGE SAT SCORE:" + " " + str(avg_SAT_score)+ " " + "You might want to take that test again..." + " " + emoji.emojize(":grimacing_face:"))
+elif avg_SAT_score <= (int(SAT_score) + 50):
+    print("AVERAGE SAT SCORE:" + " " + str(avg_SAT_score) + " " + "Worth a shot!" + " " + emoji.emojize(":relieved_face:"))
+else:
+    pass
+print("-------------------------")
+
+#Giving feedback on the diversity of the institution
+
+percent_female = float(parsed_response['results'][0]['latest.student.demographics.female_share'])
+percent_female_modified = percent_female * 100
+
+if percent_female > float(avg_percent_female):
+    print("PERCENT FEMALE:" + " " + str(round(percent_female_modified,2))+ "% " + "Large female population at this school." + " " + emoji.emojize(":woman:"))
+elif 0.5 <= percent_female <= float(avg_percent_female):
+    print("PERCENT FEMALE:" + " " + str(round(percent_female_modified,2))+ "% " + "Average sized female population at this school." + " " + emoji.emojize(":woman:"))
+else:
+    print("PERCENT FEMALE:" + " " + str(round(percent_female_modified,2))+ "% " + "Small female population at this school." + " " + emoji.emojize(":thumbs_down:"))
+
+percent_white= float(parsed_response['results'][0]['latest.student.demographics.race_ethnicity.white_2000'])
+percent_white_modified = percent_white * 100
+
+if percent_white > float(avg_percent_white):
+    print("RACIAL & ETHINIC DIVERSITY:" + " " + "This school is not very diverse." + " " + emoji.emojize(":thumbs_down:"))
+elif 0.45 <= percent_white <= float(avg_percent_white):
+    print("RACIAL & ETHINIC DIVERSITY:"  + " " + "This school is moderately diverse." + " " +emoji.emojize(":simple_smile:"))
+else:
+    print("RACIAL & ETHINIC DIVERSITY:"  + " " + "This school is very diverse." + " " +emoji.emojize(":thumbs_up:"))
+
+
 
 
 #print("REQUEST AT: " + " " + now.strftime("%Y-%m-%d %H:%M:%S"))
